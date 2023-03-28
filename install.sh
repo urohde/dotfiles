64418dd93b26b299a1db9b3bd5dac0b87ee2fdf2
urohde <ulrikro@gmail.com>
2023-03-28 14:09:03 +0200

    fixed directory linking

--- install.sh
+++ install.sh
@@ -2,21 +2,25 @@
 
 SCRIPT_DIR=$(pwd)
 
+function link_directory() {
+    [ -d "$1/." ] || ln -s $1 $2
+}
+
 # zsh
 ln -s $SCRIPT_DIR/zshrc $HOME/.zshrc
 ln -s $SCRIPT_DIR/p10k.zsh $HOME/.p10k.zsh
 
 # nvim
-ln -s $SCRIPT_DIR/config/nvim $HOME/.config/nvim
+link_directory $SCRIPT_DIR/config/nvim $HOME/.config/nvim
 
 # ranger
 #sudo apt install ranger
-ln -s $SCRIPT_DIR/config/ranger $HOME/.config/ranger
+link_directory $SCRIPT_DIR/config/ranger $HOME/.config/ranger
 #git clone https://github.com/alexanderjeurissen/ranger_devicons $HOME/.config/ranger/plugins/ranger_devicons
 
 # flameshot
-ln -s $SCRIPT_DIR/config/flameshot $HOME/.config/flameshot
+link_directory $SCRIPT_DIR/config/flameshot $HOME/.config/flameshot
 
 # corsair keyboard rgb
-ln -s $SCRIPT_DIR/config/ckb-next $HOME/.config/ckb-next
+link_directory $SCRIPT_DIR/config/ckb-next $HOME/.config/ckb-next
 
