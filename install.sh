96a30bb2ab30bacb7c5af00ff23a81cfc7614d61
urohde <ulrikro@gmail.com>
2023-03-29 18:39:09 +0200

    added i3, added rofi

--- install.sh
+++ install.sh
@@ -1,26 +1,33 @@
 #! /bin/bash
 
 SCRIPT_DIR=$(pwd)
+CONFIG_DIR=$HOME/.config
 
 function link_directory() {
-    [ -d "$1/." ] || ln -s $1 $2
+    [ ! -d $2 ] && ln -s $1 $2
 }
 
 # zsh
 ln -s $SCRIPT_DIR/zshrc $HOME/.zshrc
 ln -s $SCRIPT_DIR/p10k.zsh $HOME/.p10k.zsh
 
+# i3
+link_directory $SCRIPT_DIR/config/i3 $CONFIG_DIR/i3
+
+# rofi
+link_directory $SCRIPT_DIR/config/rofi $CONFIG_DIR/rofi
+
 # nvim
-link_directory $SCRIPT_DIR/config/nvim $HOME/.config/nvim
+link_directory $SCRIPT_DIR/config/nvim $CONFIG_DIR/nvim
 
 # ranger
 #sudo apt install ranger
-link_directory $SCRIPT_DIR/config/ranger $HOME/.config/ranger
-#git clone https://github.com/alexanderjeurissen/ranger_devicons $HOME/.config/ranger/plugins/ranger_devicons
+link_directory $SCRIPT_DIR/config/ranger $CONFIG_DIR/ranger
+#git clone https://github.com/alexanderjeurissen/ranger_devicons $CONFIG_DIR/ranger/plugins/ranger_devicons
 
 # flameshot
-link_directory $SCRIPT_DIR/config/flameshot $HOME/.config/flameshot
+link_directory $SCRIPT_DIR/config/flameshot $CONFIG_DIR/flameshot
 
 # corsair keyboard rgb
-link_directory $SCRIPT_DIR/config/ckb-next $HOME/.config/ckb-next
+link_directory $SCRIPT_DIR/config/ckb-next $CONFIG_DIR/ckb-next
 
