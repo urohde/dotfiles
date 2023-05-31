a4dde3cc0a47376dcfe743f8b9fc4dba13a98293
noen <ulrikro@gmail.com>
2023-05-31 22:21:35 +0200

    qtile: add one screen for each monitor

--- config/qtile/screens.py
+++ config/qtile/screens.py
@@ -5,12 +5,8 @@ from bars import main_bar
 
 monitors = int(subprocess.run("xrandr | grep ' connected '| wc -l", capture_output=True, shell=True).stdout.decode()[0])
 
-if monitors == 2: 
-    screens = [
-            Screen( top=main_bar ),
-            Screen(),
-            ]
-else: 
-    screens = [
-            Screen( top = main_bar)
-            ]
+screens = [Screen( top=main_bar )]
+
+for i in range(monitors - 1):
+    screens.append(Screen())
+
