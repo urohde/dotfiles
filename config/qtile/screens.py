80d68bd98e86b79135f15a1f1bba43a65fcf0369
noen <ulrikro@gmail.com>
2023-05-31 22:12:22 +0200

    qtile: dual monitor

--- config/qtile/screens.py
+++ config/qtile/screens.py
@@ -1,7 +1,16 @@
 from libqtile.config import Screen
-from libqtile import bar, widget
+import subprocess
 from bars import main_bar
 
-screens = [
-    Screen( top=main_bar ),
-]
+
+monitors = int(subprocess.run("xrandr | grep ' connected '| wc -l", capture_output=True, shell=True).stdout.decode()[0])
+
+if monitors == 2: 
+    screens = [
+            Screen( top=main_bar ),
+            Screen(),
+            ]
+else: 
+    screens = [
+            Screen( top = main_bar)
+            ]
